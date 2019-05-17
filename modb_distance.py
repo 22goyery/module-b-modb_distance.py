b5c9a1e974be7fdce5c5e2a37d0929ad59dec2cc
22goyery <49483199+22goyery@users.noreply.github.com>
2019-05-17 08:12:47 -0400

    Add files via upload

---
+++ modb_distance.py
@@ -0,0 +1,13 @@
+def main():
+    print("space key to start")
+
+    print("what converts distance measured in kilometers to miles")
+    kilometers = eval(input("what is the distance in kilometers"))
+    distance = 0.42 *0.20  * kilometers + 45
+    print("the distance is",kilometers, "miles kilometer.")
+
+   # to end the program
+   input("press the <enter> key to quit.")
+
+
+   main()
